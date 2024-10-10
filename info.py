91b124ab6c743fd838b9022c19a953a7aaf134ee
The Happy Hour <159997953+The-Happy-Hour@users.noreply.github.com>
2024-10-10 11:05:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,10 +1,10 @@
 import os
 # Bot information
-API_ID = os.getenv('API_ID', "")
-API_HASH = os.getenv('API_HASH', "")
-BOT_TOKEN = os.getenv('BOT_TOKEN', "")
+API_ID = os.getenv('API_ID', "18029060")
+API_HASH = os.getenv('API_HASH', "c7e952440251e33bb5cce566b29f7254")
+BOT_TOKEN = os.getenv('BOT_TOKEN', "7082078294:AAGWIeE1zSxBHVncVM10shxP8grw3_327rA")
 
 # stream vars
 PORT = int(os.getenv('PORT', '5050'))
-BIN_CHANNEL = os.getenv("BIN_CHANNEL", "") #Log Channel
-URL = os.getenv("URL", "") #App URL not MongoDB URL
+BIN_CHANNEL = os.getenv("BIN_CHANNEL", "-1002376421685") #Log Channel
+URL = os.getenv("URL", "https://thh-file-2-link.onrender.com") #App URL not MongoDB URL
